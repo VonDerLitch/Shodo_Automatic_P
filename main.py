2b962e86d031c97e45a1834855b0a1be00810d8c
KaioValls <vallsbusiness.acc@gmail.com>
2023-02-03 15:12:57 -0300

    Systray fixed 0.3

--- main.py
+++ main.py
@@ -5,7 +5,21 @@ from sistray import *
 
 
 while True:
-    automatic()
-    sysTrayIcon.start()
+    event, values = window.read()
+    if event == "Executar Shodô":
+        window.hide()
+        startShodo()
+        window.un_hide()
+    if event == "TUTORIAL:Como utilizar":
+        os.startfile(
+            "https://drive.google.com/file/d/18pTMRX-nY437Gh8R6P6XBUsDsOjTOob6/view")
+
+    if event == "Sair":
+        window.hide()
+
+    if event == None:
+        sysTrayIcon.shutdown()
+        exit()
+    
     
 # Qualquer problema ou duvida com o código contactar Principe ignorante(kelvin Silveira), João Pedro.
--- sistray.py
+++ sistray.py
@@ -1,42 +1,25 @@
 from win import window
-from infi.systray import SysTrayIcon
+from libs.sistrayLib import SysTrayIcon
 import os
 from shodo import startShodo
-
-
-def automatic():
-    event, values = window.read()
-    if event == "Executar Shodô":
-        window.hide()
-        startShodo()
-        window.un_hide()
-    if event == "TUTORIAL:Como utilizar":
-        os.startfile(
-            "https://drive.google.com/file/d/18pTMRX-nY437Gh8R6P6XBUsDsOjTOob6/view")
-
-    if event == "Sair":
-        window.hide()
-
-    if event == None:
-        exit()
-
-
+    
 hover_text = "Shodo Automatic Demo"
-
 
 def open(sysTrayIcon):
     window.un_hide()
 
 
 def shodo(sysTrayIcon):
-    window.un_hide()
     window.hide()
     startShodo()
+    
 
 
-menu_options = (('Abrir', "Da p colocar icone aqui se quiser", open),
+menu_options = (('Abrir', None, open),
                 ('Executar Shodo', None, shodo),
 
                 )
 sysTrayIcon = SysTrayIcon("iconn.ico", hover_text,
-                          menu_options, on_quit=None, default_menu_index=1)
+                          menu_options, default_menu_index=1)
+                          
+sysTrayIcon.start()
